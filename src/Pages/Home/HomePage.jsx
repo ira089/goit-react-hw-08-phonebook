355772e3ee789e0e16cbc2ebf3b05f89e3fd7c85
ira089 <gvir2015@gmail.com>
2024-02-17 22:43:58 +0200

    react_HW8_ img

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import styles from '../Layout/layout.module.css';
 
 const HomePage = () => {
-  return <h1>Phonebook</h1>;
-  // return <img src="../../img/6532.webp" alt="phone" />;
+  // return <h1>Phonebook</h1>;
+  return (
+    <div className={styles.wrap}>
+      <h1>Phonebook</h1>
+    </div>
+  );
+  // <img src="../../img/6532.webp" alt="phone" />;
 };
 
 export default HomePage;
